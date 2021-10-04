e572f185e11c2684d7e7b5c00aa5f44f2ca94a79
rafael-branco <rafael.rbranco@hotmail.com>
2021-10-04 14:52:40 -0300

    Create ReverseWords.py

---
+++ ReverseWords.py
@@ -0,0 +1,16 @@
+def special_reverse(str, letter):
+    temp = str.lower()
+    temp = temp.split(" ")
+    for i in range(0, len(temp)):
+        if temp[i][0] == letter:
+            temp[i] = temp[i][::-1]
+    
+    newStr = " "
+
+    newStr = newStr.join(temp,)
+    
+    return newStr
+
+print(special_reverse("word searches are super fun", "s"))
+
+
